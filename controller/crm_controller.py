cabd1acdba6e8d834323087a95e78117bfbadac2
Wojciech Wesołowski <wojtek.wes@gmail.com>
2020-07-12 19:36:05 +0200

    Add new functionalities to crm_controller.py
    New features:
    - add a separate function (capitalize_list_elements) that
      capitalizes all elements in the list;
    - add a function (get_customer_lp) that returns a user-specified
      Lp of the customer;
    - add a global variable (msg) that is printed just below the menu
      whenever some error occurs.
    
    Modified functions:
    1) add_customer():
      - add a print message;
      - apply the function capitalize_list_elements().
    2) update_customer():
      - apply the function get_customer_lp();
      - a user can see the current values of a customer’s data;
      - if a user omits the input the current data value will not
        change;
    3) delete_customer():
      - apply the function get_customer_lp().

--- controller/crm_controller.py
+++ controller/crm_controller.py
@@ -2,8 +2,15 @@ from model.crm import crm
 from view import terminal as view
 
 
+msg = ""
+
+def capitalize_list_elements(data):
+    return list(map(lambda element : element.capitalize(), data))
+    
+
 def list_customers():
     # view.print_error_message("Not implemented yet.")
+    view.print_message("\nThe list of customers:")
     customers = crm.get_customers()
     
     for index, customer in enumerate(customers):
@@ -21,7 +28,10 @@ def list_customers():
 
 def add_customer():
     # view.print_error_message("Not implemented yet.")
-    new_customer_data = view.get_inputs(crm.HEADERS[1:])
+    view.print_message("\nPlease, provide the new customer's data:")
+    headers = capitalize_list_elements(crm.HEADERS[1:])
+    new_customer_data = view.get_inputs(headers)
+
     crm.add_customer(new_customer_data)
 
 
@@ -37,30 +47,63 @@ def clean_line():
     print("\033[0K", end="")
 
 
-def update_customer():
-    # view.print_error_message("Not implemented yet.")
+def get_customer_lp(action_type):
+    global msg
     
-    # header = crm.HEADERS[1:]
-    # header.insert(0, "Lp")
     entries_number = crm.get_entries_number()
     
     if entries_number == 0:
         view.print_error_message("There is no entries to update.")
         return
     
-    header = ["Lp"]
-    header.extend(crm.HEADERS[1:])
-    header = list(map(lambda element : element.capitalize(), header))
+    customer_lp = view.get_input(f"\nPlease, provide the Lp number of the customer whose data you want to {action_type} or [c] to cancel")
+    
+    if customer_lp == "":
+        msg = "You must provide a digit."
+    elif customer_lp == "c":
+        return None
+    elif not customer_lp.isdigit():
+        msg = "You must provide a digit."
+        return None
+    else:
+        customer_lp = int(customer_lp)
+        
+    if customer_lp not in range(1, entries_number + 1):
+        msg = f"The Lp number must be in the range between: {1} and {entries_number}"
+        return None
+    
+    return customer_lp
+
+
+def update_customer():
+    global msg
+    NAME_INDEX = 1
+    
+    all_customers = crm.get_customers()
+    customer_lp = get_customer_lp("update")
+    if customer_lp is None:
+        return
+    
+    view.print_message("\nPlease, provide updated customer's data.")
+    view.print_message("Press ENTER if you want to keep the value.")
+    
+    header = crm.HEADERS[NAME_INDEX:]
+    header = capitalize_list_elements(header)
+    
+    for index, customer_data in enumerate(all_customers[customer_lp - 1][NAME_INDEX:]):
+        header[index] += f" ({customer_data})"
     
     update_customer_data = None
     while update_customer_data is None:
         print("")
-        update_customer_data = view.get_inputs(header)
+        update_customer_data = [str(customer_lp)]
+        update_customer_data.extend(view.get_inputs(header))
         
-        if update_customer_data[0] not in range(1, entries_number + 1):
-            view.print_error_message(f"The Lp number must be in the range between: {1} and {entries_number}")
-            continue
+        for index, value in enumerate(update_customer_data):
+            if value == "":
+                update_customer_data[index] = all_customers[customer_lp - 1][index + NAME_INDEX]
         
+        header.insert(0, "Lp")
         update_customer_data_table = [header, update_customer_data]
         print("")
         view.print_table(update_customer_data_table)
@@ -87,30 +130,19 @@ def update_customer():
 
 def delete_customer():
     # view.print_error_message("Not implemented yet.")
-    customer_lp = None
+    global msg
+    customer_lp = get_customer_lp("delete")
     
-    while customer_lp is None:
-        customer_lp = view.get_input("Provide the lp of the customer to delete or input [c] to cancel")
-        
-        if customer_lp == "":
-            customer_lp = None
-        elif not customer_lp.isdigit():
-            customer_lp = None
-            view.print_error_message("The customer lp must be a digit.")
-        elif customer_lp == "c":
-            pass
-        else:
-            try:
-                crm.delete_customer(customer_lp)
-            except IndexError:
-                view.print_error_message("Customer lp out of range.")
-                customer_lp = None
+    if customer_lp is None:
+        return
+    
+    crm.delete_customer(customer_lp)
 
 
 def get_subscribed_emails():
     # view.print_error_message("Not implemented yet.")
     emails = crm.get_subscribed_emails()
-    view.print_general_results(emails, "Subscribed emails")
+    view.print_general_results(emails, "\nSubscribed emails")
 
 
 def run_operation(option):
@@ -141,9 +173,14 @@ def display_menu():
 
 
 def menu():
+    global msg
     operation = None
     while operation != '0':
         display_menu()
+        if msg != "":
+            view.print_error_message(msg)
+            msg = ""
+            
         try:
             operation = view.get_input("Select an operation")
             run_operation(int(operation))
