af01dd650675e3785895605da2d6866adf9fcec3
Aviatore <wojtek.wes@gmail.com>
2020-07-14 12:38:49 +0200

    Refaktor the file model/crud.py

--- model/crud.py
+++ model/crud.py
@@ -3,23 +3,30 @@ from model import data_manager, util
 
 def copy_nested_list(data):
     new_list = []
+
     for line in data:
         tmp = []
+
         for element in line:
             tmp.append(element)
+
         new_list.append(tmp)
+
     return new_list
 
 
 def function_get(data):
     new_data = copy_nested_list(data)
+
     return new_data
 
 
 def function_add(data, data_file, new_data):
     new_id = util.generate_id()
+
     data_new_customer = [new_id]
     data_new_customer.extend(new_data)
+    
     data.append(data_new_customer)
 
     data_manager.write_table_to_file(data_file, data)
