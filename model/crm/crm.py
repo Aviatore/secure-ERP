13d68bd7b789d3691a29bd082ec947defb2733d3
Aviatore <wojtek.wes@gmail.com>
2020-07-15 11:37:24 +0200

    Implement the function to get emails - crm.py

--- model/crm/crm.py
+++ model/crm/crm.py
@@ -12,3 +12,14 @@ from model import data_manager, util
 
 DATAFILE = "model/crm/crm.csv"
 HEADERS = ["id", "name", "email", "subscribed"]
+
+data = data_manager.read_table_from_file(DATAFILE)
+
+def get_subscribed_emails():
+    emails = []
+    
+    for customer in data:
+        if customer[3] == "1":
+            emails.append(customer[2])
+    
+    return emails
